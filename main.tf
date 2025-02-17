b0113f009b2595ee4001776dd4f950ec43d0b78f
jwkidd3 <jkidd@kiddcorp.com>
2025-02-17 14:09:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-}
+
 
 # 🔹 Lookup the Default VPC
 data "aws_vpc" "default" {
